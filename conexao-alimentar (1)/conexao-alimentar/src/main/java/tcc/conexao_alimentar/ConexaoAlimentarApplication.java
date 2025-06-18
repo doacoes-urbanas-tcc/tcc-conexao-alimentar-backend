8921a27943dcefac057760aef2318b2810c841a8
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-06-18 16:03:33 -0300

    feat: Adiciona workflow de CI

---
+++ conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/ConexaoAlimentarApplication.java
@@ -0,0 +1,13 @@
+package tcc.conexao_alimentar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexaoAlimentarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexaoAlimentarApplication.class, args);
+	}
+
+}
---
+++ conexao-alimentar (1)/conexao-alimentar/src/test/java/tcc/conexao_alimentar/ConexaoAlimentarApplicationTests.java
@@ -0,0 +1,13 @@
+package tcc.conexao_alimentar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexaoAlimentarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
