7d839e4c38b8df5d8edfc9f3a369b122ac34644d
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-06-28 08:47:36 -0300

    feat: adiciona enum CategoriaAlimento ao módulo de doações

---
+++ conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/enums/CategoriaAlimento.java
@@ -0,0 +1,16 @@
+package tcc.conexao_alimentar.enums;
+
+public enum CategoriaAlimento {
+
+    HORTIFRUTI,
+    PADARIA,
+    GRÃOS,
+    CARNES,
+    LATICÍNIOS,
+    BEBIDAS,
+    CONSERVAS,
+    MASSAS,
+    DOCES,
+    OUTROS;
+
+}
