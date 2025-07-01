8efb4c10ce4a9af4e648a9a6a64c195acc499368
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-07-01 13:57:42 -0300

    feat: adiciona os métodos de busca no repository de usuário

--- conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/repository/UsuarioRepository.java
+++ conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/repository/UsuarioRepository.java
@@ -1,9 +1,17 @@
 package tcc.conexao_alimentar.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import tcc.conexao_alimentar.model.UsuarioModel;
 
 public interface UsuarioRepository  extends JpaRepository<UsuarioModel,Long>{
 
+    Optional<UsuarioModel> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    List<UsuarioModel> findByAtivoFalse();
 }
