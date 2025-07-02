c2d1cad4f7193afb1dc168573994a724a7faeed8
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-07-02 11:54:38 -0300

    fix: deleta AdministradorModel e AdministradorRepository

--- conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/model/AdministradorModel.java
+++
@@ -1,9 +0,0 @@
-package tcc.conexao_alimentar.model;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
-@Entity@Table(name = "administrador")
-public class AdministradorModel  extends UsuarioModel{
-
-}
