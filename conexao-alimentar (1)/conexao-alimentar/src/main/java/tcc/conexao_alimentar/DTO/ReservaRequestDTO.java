494c25402ad9530e5657b0f623b507f874af7273
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-07-05 16:20:49 -0300

    feat: adiciona ReservaRequestDTO

---
+++ conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/DTO/ReservaRequestDTO.java
@@ -0,0 +1,11 @@
+package tcc.conexao_alimentar.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ReservaRequestDTO {
+
+    private Long doacaoId;
+}
