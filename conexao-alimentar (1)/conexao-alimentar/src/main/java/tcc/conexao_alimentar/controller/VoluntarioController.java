157c5658a7ff47783061cd358cb0a34d00c014f0
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-07-06 17:24:52 -0300

    feat: adiciona endponit de POST em VoluntarioController para cadastrar veículo caso o voluntário escolha ser transportador

--- conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/controller/VoluntarioController.java
+++ conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/controller/VoluntarioController.java
@@ -3,7 +3,10 @@ package tcc.conexao_alimentar.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import tcc.conexao_alimentar.DTO.VeiculoRequestDTO;
 import tcc.conexao_alimentar.DTO.VoluntarioRequestDTO;
+import tcc.conexao_alimentar.service.VeiculoService;
 import tcc.conexao_alimentar.service.VoluntarioService;
 
 @RestController
@@ -12,11 +15,21 @@ import tcc.conexao_alimentar.service.VoluntarioService;
 public class VoluntarioController {
 
     private final VoluntarioService voluntarioService;
+    private final VeiculoService veiculoService;
 
     @PostMapping("/cadastrar")
     public ResponseEntity<String> cadastrar(@RequestBody VoluntarioRequestDTO dto) {
         voluntarioService.cadastrar(dto);
         return ResponseEntity.ok("Voluntário cadastrado com sucesso! Aguarde aprovação.");
     }
+
+    @PostMapping("/{voluntarioId}/veiculo")
+    public ResponseEntity<String> cadastrarVeiculo(@PathVariable Long voluntarioId,@RequestBody VeiculoRequestDTO dto) {
+        dto.setVoluntarioId(voluntarioId);  
+        veiculoService.cadastrarVeiculo(dto);
+        return ResponseEntity.ok("Veículo cadastrado com sucesso para voluntário!");
+    }
+
+   
 
 }
