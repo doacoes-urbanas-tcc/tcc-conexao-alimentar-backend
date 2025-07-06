fca3c535bfc44fc0370f6f47bfb3af7951ee747a
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-07-06 18:12:20 -0300

    feat: adiciona notações de cocumentação básica no DoacaoController

--- conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/controller/DoacaoController.java
+++ conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/controller/DoacaoController.java
@@ -3,6 +3,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import tcc.conexao_alimentar.DTO.DoacaoRequestDTO;
 import tcc.conexao_alimentar.DTO.DoacaoResponseDTO;
 import tcc.conexao_alimentar.service.DoacaoService;
@@ -12,10 +17,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/doacoes")
 @RequiredArgsConstructor
+@Tag(name = "Doações", description = "Endpoints para gerenciamento de doações")
 public class DoacaoController {
 
     private final DoacaoService service;
-
+    
+    @Operation(summary = "Cadastrar nova doação",description = "Permite que um usuário do tipo COMERCIO, PRODUTOR_RURAL ou PESSOA_FISICA cadastre uma nova doação.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Doação cadastrada com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Dados inválidos"),
+        @ApiResponse(responseCode = "401", description = "Credenciais de autenticação inválidas"),
+        @ApiResponse(responseCode = "403", description = "Acesso não autorizado"),
+        @ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
+    })
     @PostMapping
     @PreAuthorize("hasAuthority('ROLE_COMERCIO') or hasAuthority('ROLE_PRODUTOR_RURAL') or hasAuthority('ROLE_PESSOA_FISICA')")
     public ResponseEntity<?> criar(@RequestBody DoacaoRequestDTO dto) {
@@ -23,6 +37,13 @@ public class DoacaoController {
         return ResponseEntity.ok("Doação cadastrada com sucesso!");
     }
 
+    @Operation(summary = "Listar todas as doações",description = "Retorna uma lista de todas as doações disponíveis. Somente ONG, VOLUNTÁRIO ou ADMIN podem acessar.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Lista de doações retornada com sucesso"),
+        @ApiResponse(responseCode = "403", description = "Acesso não autorizado"),
+        @ApiResponse(responseCode = "404", description = "Recurso não encontrado")
+
+    })
     @GetMapping
     @PreAuthorize("hasAuthority('ROLE_ONG') or hasAuthority('ROLE_VOLUNTARIO') or hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<List<DoacaoResponseDTO>> listarTodas() {
@@ -30,6 +51,14 @@ public class DoacaoController {
         return ResponseEntity.ok(lista);
     }
 
+    
+    @Operation(summary = "Listar doação por id",description = "Retorna a doação buscada. Somente ONG, VOLUNTÁRIO ou ADMIN podem acessar.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Doação retornada com sucesso"),
+        @ApiResponse(responseCode = "403", description = "Acesso não autorizado"),
+        @ApiResponse(responseCode = "404", description = "Recurso não encontrado")
+
+    })
     @GetMapping("/{id}")
     @PreAuthorize("hasAuthority('ROLE_ONG') or hasAuthority('ROLE_VOLUNTARIO') or hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<DoacaoResponseDTO> buscarPorId(@PathVariable Long id) {
@@ -37,6 +66,14 @@ public class DoacaoController {
     return ResponseEntity.ok(dto);
     }
 
+    @Operation(summary = "Deleta doação por id",description = "Deleta a doação selecionada. Somente COMERCIO, PRODUTOR_RURAL, PESSOA_FISICA e ADMIN podem acessar.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "Doação removida com sucesso"),
+        @ApiResponse(responseCode = "401", description = "Credenciais de autenticação inválidas"),
+        @ApiResponse(responseCode = "403", description = "Acesso não autorizado"),
+        @ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
+
+    })
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAnyRole('ADMIN', 'COMERCIO', 'PRODUTOR_RURAL', 'PESSOA_FISICA')")
     public ResponseEntity<Void> remover(@PathVariable Long id, @RequestParam Long doadorId) {
