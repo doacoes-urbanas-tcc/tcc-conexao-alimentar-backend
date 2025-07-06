df3436a563a37346e23cd323dcd5fbe453ba12c2
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-07-06 18:17:11 -0300

    feat: adiciona documentação básica no UsuarioController

--- conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/controller/UsuarioController.java
+++ conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/controller/UsuarioController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import tcc.conexao_alimentar.enums.TipoUsuario;
 import tcc.conexao_alimentar.model.UsuarioModel;
@@ -24,6 +27,15 @@ public class UsuarioController {
 
     private final UsuarioRepository usuarioRepository;
 
+    
+    @Operation(summary = "Listar os cadastros pendentes",description = "Lista todos os cadastros pendentes de ativação. Somente ADMIN pode acessar")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Doação cadastrada com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Dados inválidos"),
+        @ApiResponse(responseCode = "401", description = "Credenciais de autenticação inválidas"),
+        @ApiResponse(responseCode = "403", description = "Acesso não autorizado"),
+        @ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
+    })
     @GetMapping("/pendentes")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> listarPendentes() {
@@ -31,6 +43,14 @@ public class UsuarioController {
         return ResponseEntity.ok(pendentes);
     }
 
+    @Operation(summary = "Listar os cadastros pendentes por tipo",description = "Lista todos os cadastros pendentes de ativação de um determinado tipo de usuário. Somente ADMIN pode acessar")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Doação cadastrada com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Dados inválidos"),
+        @ApiResponse(responseCode = "401", description = "Credenciais de autenticação inválidas"),
+        @ApiResponse(responseCode = "403", description = "Acesso não autorizado"),
+        @ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
+    })
     @GetMapping("/pendentes/{tipo}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> listarPendentesPorTipo() {
@@ -40,6 +60,14 @@ public class UsuarioController {
     return ResponseEntity.ok(agrupados);
 }
 
+ @Operation(summary = "Permite aprovar cadastro por id",description = "Permite aprovar um cadastro pendente pelo id. Somente ADMIN pode acessar")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Doação cadastrada com sucesso"),
+        @ApiResponse(responseCode = "400", description = "Dados inválidos"),
+        @ApiResponse(responseCode = "401", description = "Credenciais de autenticação inválidas"),
+        @ApiResponse(responseCode = "403", description = "Acesso não autorizado"),
+        @ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
+    })
 
     @PatchMapping("/aprovar/{id}")
     @PreAuthorize("hasRole('ADMIN')")
