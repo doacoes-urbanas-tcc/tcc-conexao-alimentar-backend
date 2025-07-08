66faff0dd3e82875d1ad4935fe8a60687c531f29
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-07-08 13:45:11 -0300

    feat: cria enum SetorTi e VoluntarioTiModel e atualiza SetorAtuacao - Cria enum SetorTi para categorizar as diferentes áreas de atuação que um voluntário pode escolher ao se cadastrar como voluntário de Ti no sistema - Cria VoluntarioTiModel para coletar informações e qualificações do candidato a voluntariado

---
+++ conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/model/VoluntarioTiModel.java
@@ -0,0 +1,33 @@
+package tcc.conexao_alimentar.model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import tcc.conexao_alimentar.enums.SetorTi;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "voluntario_ti")
+public class VoluntarioTiModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private SetorTi setorTi;
+    private String stackConhecimento;
+    private String certificacoes;
+    private String experiencia;
+    private String linkedin;
+    private String github;
+    private Double disponibilidadeHoras;
+        
+
+
+}
