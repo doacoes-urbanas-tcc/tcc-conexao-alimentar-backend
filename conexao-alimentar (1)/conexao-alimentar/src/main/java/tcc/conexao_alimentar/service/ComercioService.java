6d3e60bacbce4cc3b93772de15c27277c6ccafbd
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-07-08 14:52:31 -0300

    feat: adiciona métodos para atualizar perfil, atualizar senha e visualizar perfil no service de comércio

--- conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/service/ComercioService.java
+++ conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/service/ComercioService.java
@@ -2,10 +2,15 @@ package tcc.conexao_alimentar.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 import tcc.conexao_alimentar.DTO.ComercioRequestDTO;
+import tcc.conexao_alimentar.DTO.ComercioResponseDTO;
 import tcc.conexao_alimentar.enums.TipoUsuario;
+import tcc.conexao_alimentar.exception.RegraDeNegocioException;
 import tcc.conexao_alimentar.mapper.ComercioMapper;
 import tcc.conexao_alimentar.model.ComercioModel;
+import tcc.conexao_alimentar.model.UsuarioModel;
 import tcc.conexao_alimentar.repository.ComercioRepository;
 
 @Service
@@ -14,6 +19,7 @@ public class ComercioService {
 
     private final ComercioRepository comercioRepository;
     private final PasswordEncoder passwordEncoder;
+    private UsuarioService usuarioService;
 
     public void cadastrar(ComercioRequestDTO dto) {
         ComercioModel model = ComercioMapper.toEntity(dto);
@@ -22,5 +28,27 @@ public class ComercioService {
         model.setAtivo(false);
         comercioRepository.save(model);
     }
+
+    @Transactional
+    public void atualizarEmail(Long comercioId, String novoEmail) {
+        usuarioService.atualizarEmail(comercioId, novoEmail);
+    }
+
+    @Transactional
+    public void atualizarSenha(Long comercioId, String novaSenha) {
+        usuarioService.atualizarSenha(comercioId, novaSenha, passwordEncoder);
+    }
+
+    public ComercioResponseDTO visualizarPerfil(Long id) {
+    UsuarioModel usuario = usuarioService.buscarPorId(id)
+     .orElseThrow(() -> new RegraDeNegocioException("Usuário não encontrado."));
+
+    if (!(usuario instanceof ComercioModel)) {
+        throw new RegraDeNegocioException("Usuário não é um comércio.");
+    }
+
+    ComercioModel comercio = (ComercioModel) usuario;
+    return ComercioMapper.toResponse(comercio);
+}
 
 }
