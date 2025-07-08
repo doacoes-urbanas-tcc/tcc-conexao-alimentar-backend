5684e7ad2b2e7b7d869783ec8b46d0be8516db6c
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-07-08 15:02:11 -0300

    feat: adiciona métodos de atualização no service de produtor rural

--- conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/service/ProdutorRuralService.java
+++ conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/service/ProdutorRuralService.java
@@ -2,10 +2,18 @@ package tcc.conexao_alimentar.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import tcc.conexao_alimentar.DTO.PessoaFisicaResponseDTO;
 import tcc.conexao_alimentar.DTO.ProdutorRuralRequestDTO;
+import tcc.conexao_alimentar.DTO.ProdutorRuralResponseDTO;
 import tcc.conexao_alimentar.enums.TipoUsuario;
+import tcc.conexao_alimentar.exception.RegraDeNegocioException;
+import tcc.conexao_alimentar.mapper.PessoaFisicaMapper;
 import tcc.conexao_alimentar.mapper.ProdutorRuralMapper;
+import tcc.conexao_alimentar.model.PessoaFisicaModel;
 import tcc.conexao_alimentar.model.ProdutorRuralModel;
+import tcc.conexao_alimentar.model.UsuarioModel;
 import tcc.conexao_alimentar.repository.ProdutorRuralRepository;
 
 @Service
@@ -14,6 +22,7 @@ public class ProdutorRuralService {
 
     private final ProdutorRuralRepository produtorRuralRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UsuarioService usuarioService;
 
     public void cadastrar(ProdutorRuralRequestDTO dto) {
         ProdutorRuralModel model = ProdutorRuralMapper.toEntity(dto);
@@ -22,5 +31,29 @@ public class ProdutorRuralService {
         model.setAtivo(false);
         produtorRuralRepository.save(model);
     }
+
+    
+    @Transactional
+    public void atualizarEmail(Long prId, String novoEmail) {
+        usuarioService.atualizarEmail(prId, novoEmail);
+    }
+
+    @Transactional
+    public void atualizarSenha(Long prId, String novaSenha) {
+        usuarioService.atualizarSenha(prId, novaSenha, passwordEncoder);
+    }
+
+    public ProdutorRuralResponseDTO visualizarPerfil(Long id) {
+    UsuarioModel usuario = usuarioService.buscarPorId(id)
+     .orElseThrow(() -> new RegraDeNegocioException("Usuário não encontrado."));
+
+    if (!(usuario instanceof ProdutorRuralModel)) {
+        throw new RegraDeNegocioException("Usuário não é um produtor rural.");
+    }
+
+    ProdutorRuralModel pr= (ProdutorRuralModel) usuario;
+    return ProdutorRuralMapper.toResponse(pr);
+}
+    
 
 }
