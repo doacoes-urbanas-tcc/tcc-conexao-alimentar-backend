a50ca7c8f5c65916addbad887ed09cefe60fdc30
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-07-08 13:49:07 -0300

    feat: cria os DTO's e o repository de VoluntarioTi

---
+++ conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/repository/VoluntarioTiRepository.java
@@ -0,0 +1,9 @@
+package tcc.conexao_alimentar.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tcc.conexao_alimentar.model.VoluntarioTiModel;
+
+public interface VoluntarioTiRepository  extends JpaRepository<VoluntarioTiModel,Long>{
+
+}
