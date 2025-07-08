42f18baf52533fb915bdca9ddf1410032e1873b7
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-07-08 13:13:42 -0300

    feat: adiciona validações, regex e obrigatoriedade em VeiculoRequestDTO

--- conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/DTO/VeiculoRequestDTO.java
+++ conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/DTO/VeiculoRequestDTO.java
@@ -1,5 +1,8 @@
 package tcc.conexao_alimentar.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,10 +11,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class VeiculoRequestDTO {
+    
+    @NotBlank(message = "Placa é obrigatória.")
+    @Pattern(
+        regexp = "^[A-Z]{3}-\\d{4}$|^[A-Z]{3}\\d[A-Z]\\d{2}$",
+        message = "Placa deve estar no formato válido (ex: ABC-1234 ou ABC1D23)."
+    )
     private String placa;
+    @NotBlank(message = "Modelo é obrigatório.")
     private String modelo;
+    @NotBlank(message = "Modelo é obrigatório.")
     private String cor;
+    @NotBlank(message = "Capacidade de carga é obrigatória.")
+    @Pattern(
+        regexp = "^[0-9]+(kg|KG|Kg|ton|TON|Ton)$",
+        message = "Capacidade deve incluir unidade (ex: 500kg ou 1ton)."
+    )
     private String capacidadeCarga;
+    @NotNull(message = "ID do voluntário é obrigatório.")
     private Long voluntarioId;
 
 }
