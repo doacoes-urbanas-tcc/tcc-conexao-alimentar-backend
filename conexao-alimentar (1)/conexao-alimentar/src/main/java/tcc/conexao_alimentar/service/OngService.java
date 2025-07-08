8728edfc26a2accf7c8c66473834cf67d88f4fe2
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-07-08 14:55:44 -0300

    feat: adiciona métodos de atualizar email e senha e visualizar perfil no service de ong

--- conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/service/OngService.java
+++ conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/service/OngService.java
@@ -2,10 +2,18 @@ package tcc.conexao_alimentar.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import tcc.conexao_alimentar.DTO.ComercioResponseDTO;
 import tcc.conexao_alimentar.DTO.OngRequestDTO;
+import tcc.conexao_alimentar.DTO.OngResponseDTO;
 import tcc.conexao_alimentar.enums.TipoUsuario;
+import tcc.conexao_alimentar.exception.RegraDeNegocioException;
+import tcc.conexao_alimentar.mapper.ComercioMapper;
 import tcc.conexao_alimentar.mapper.OngMapper;
+import tcc.conexao_alimentar.model.ComercioModel;
 import tcc.conexao_alimentar.model.OngModel;
+import tcc.conexao_alimentar.model.UsuarioModel;
 import tcc.conexao_alimentar.repository.OngRepository;
 
 @Service
@@ -14,6 +22,7 @@ public class OngService {
 
     private final OngRepository ongRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UsuarioService usuarioService;
 
     public void cadastrar(OngRequestDTO dto) {
         OngModel model = OngMapper.toEntity(dto);
@@ -22,5 +31,29 @@ public class OngService {
         model.setAtivo(false);
         ongRepository.save(model);
     }
+
+    
+    @Transactional
+    public void atualizarEmail(Long ongId, String novoEmail) {
+        usuarioService.atualizarEmail(ongId, novoEmail);
+    }
+
+    @Transactional
+    public void atualizarSenha(Long ongId, String novaSenha) {
+        usuarioService.atualizarSenha(ongId, novaSenha, passwordEncoder);
+    }
+
+    public OngResponseDTO visualizarPerfil(Long id) {
+    UsuarioModel usuario = usuarioService.buscarPorId(id)
+     .orElseThrow(() -> new RegraDeNegocioException("Usuário não encontrado."));
+
+    if (!(usuario instanceof OngModel)) {
+        throw new RegraDeNegocioException("Usuário não é uma ong.");
+    }
+
+    OngModel ong = (OngModel) usuario;
+    return OngMapper.toResponse(ong);
+}
+
 
 }
