8e66867985c659da83ca961ee28846a3c0a07e04
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-07-12 19:13:13 -0300

    feat: adiciona métodos no repository de avaliação e adiciona o mapper

--- conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/repository/AvaliacaoRepository.java
+++ conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/repository/AvaliacaoRepository.java
@@ -1,9 +1,16 @@
 package tcc.conexao_alimentar.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import tcc.conexao_alimentar.model.AvaliacaoModel;
 
 public interface AvaliacaoRepository extends JpaRepository<AvaliacaoModel,Long>{
+
+    
+    List<AvaliacaoModel> findByAvaliadoId(Long avaliadoId);
+
+    List<AvaliacaoModel> findByAvaliadorId(Long avaliadorId);
 
 }
