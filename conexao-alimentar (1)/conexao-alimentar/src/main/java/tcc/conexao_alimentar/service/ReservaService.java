91b69ef6a529664d9448d55f07895cb67a39ca0b
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-07-14 20:31:18 -0300

    chore: remove application.properties do versionamento e adiciona ao .gignore

--- conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/service/ReservaService.java
+++ conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/service/ReservaService.java
@@ -3,7 +3,6 @@ package tcc.conexao_alimentar.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
