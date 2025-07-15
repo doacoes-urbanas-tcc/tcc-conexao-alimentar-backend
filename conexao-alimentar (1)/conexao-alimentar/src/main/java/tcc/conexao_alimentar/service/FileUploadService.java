b2deddf1cebaf0ad8b37eded318ae9c62e06ae25
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-07-15 17:29:50 -0300

    feat: refatora FileUploadService para suportar subpastas e UUID e exclui o controller próprio

--- conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/service/FileUploadService.java
+++ conexao-alimentar (1)/conexao-alimentar/src/main/java/tcc/conexao_alimentar/service/FileUploadService.java
@@ -3,6 +3,7 @@ package tcc.conexao_alimentar.service;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,22 +13,26 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class FileUploadService {
-    private final String uploadDir = "static/imagens/";
+    
+    private final String rootDir = "static/imagens/";
 
-    public String salvarArquivo(MultipartFile file) throws IOException {
-        File dir = new File(uploadDir);
+    public String salvarArquivo(MultipartFile file, String subpasta) throws IOException {
+        String pasta = rootDir + subpasta + "/";
+
+        File dir = new File(pasta);
         if (!dir.exists()) {
             dir.mkdirs();
         }
 
-        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
-        File uploadedFile = new File(uploadDir + fileName);
+        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        File uploadedFile = new File(pasta + fileName);
 
         try (FileOutputStream fos = new FileOutputStream(uploadedFile)) {
             fos.write(file.getBytes());
         }
 
-        return "/imagens/" + fileName;
+        return "/imagens/" + subpasta + "/" + fileName;
     }
-
 }
+
+
