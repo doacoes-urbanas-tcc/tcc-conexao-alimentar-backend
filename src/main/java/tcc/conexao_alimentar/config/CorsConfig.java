b39ad75458361b29d61be9c4be8fcb5c6424a851
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-08-09 13:48:01 -0300

    refactor: altera o corsorign para liberar o domínio do site

--- src/main/java/tcc/conexao_alimentar/config/CorsConfig.java
+++ src/main/java/tcc/conexao_alimentar/config/CorsConfig.java
@@ -4,16 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class CorsConfig {
-     @Bean
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@org.springframework.lang.NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOriginPatterns("*") 
+                    .allowedOriginPatterns("https://conexao-alimentar.netlify.app/") 
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
