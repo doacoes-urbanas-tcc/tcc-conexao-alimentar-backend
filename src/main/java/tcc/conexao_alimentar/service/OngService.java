01800a581644d7a744ff274315f2da0c5a9445b0
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-08-11 14:55:14 -0300

    fix: corrige erros

--- src/main/java/tcc/conexao_alimentar/service/OngService.java
+++ src/main/java/tcc/conexao_alimentar/service/OngService.java
@@ -62,15 +62,14 @@ public class OngService {
     }
      public Optional<OngModel> buscarPorId(Long id) {
         return ongRepository.findById(id);
-    }
-    public OngdashboardDTO getEstatisticas(Long ongId) {
-        Long totalDoacoes = reservaRepository.countDoacoesRecebidasByOng(ongId);
-        Double mediaAvaliacoes = avaliacaoRepository.findMediaAvaliacoesByOng(ongId);
-        Long receptorId = reservaRepository.findByReceptorId(ongId);
-        String nome = usuarioService.buscarNomePorId(receptorId);
+    }public OngdashboardDTO getEstatisticas(Long ongId) {
+    Long totalDoacoes = reservaRepository.countDoacoesRecebidasByOng(ongId);
+    Double mediaAvaliacoes = avaliacaoRepository.findMediaAvaliacoesByOng(ongId);
+    String nome = usuarioService.buscarNomePorId(ongId);
 
-        return new OngdashboardDTO(nome,totalDoacoes, mediaAvaliacoes != null ? mediaAvaliacoes : 0.0);
-    }
+    return new OngdashboardDTO(nome, totalDoacoes, mediaAvaliacoes != null ? mediaAvaliacoes : 0.0);
+}
+
 
 
 }
